035505d35c763e6ebac60d0992bc8432b7d37f52
Zaiyan Xu <zxu43@illinois.edu>
2019-01-25 00:26:03 -0600

    file submission

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -13,6 +13,28 @@
  * part2-functions.c in order.)
  */
 int main() {
-    // your code here
+    //1
+    first_step(81);
+
+    //2
+    int* input_2 = malloc(sizeof(int));
+    *input_2 = 132;
+    second_step(input_2);
+    free(input_2);
+
+    //3
+    int** input_3 = malloc(sizeof(int*));
+    *input_3 = malloc(sizeof(int));
+    **input_3 = 8942;
+    double_step(input_3);
+    free(*input_3);
+    free(input_3);
+    
+    //4
+    int* input_4 = malloc(sizeof(int));
+    *input_4 = 15; 
+    strange_step(((char*) input_4) - 5);
+    free(input_4);
+    
     return 0;
 }
