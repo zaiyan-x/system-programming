afbd011e3c939280c8c5a06f00b4b6678c88654e
Zaiyan Xu <zxu43@illinois.edu>
2019-02-03 14:11:13 -0600

    file submission

--- vector/sstring.c
+++ vector/sstring.c
@@ -14,22 +14,44 @@
 #include <string.h>
 
 struct sstring {
-    // Anything you want
+    vector* str_vec;
 };
 
 sstring *cstr_to_sstring(const char *input) {
     // your code goes here
-    return NULL;
+	sstring* result = malloc(sizeof(sstring));
+	result->str_vec = vector_create(char_copy_constructor, char_destructor, char_default_constructor);
+	
+	char curr;
+	size_t i;
+	for (i = 0; i < strlen(input); i++) {
+		curr = input[i];
+		vector_push_back(result->str_vec, &curr);
+	}	
+    return result;
 }
 
 char *sstring_to_cstr(sstring *input) {
     // your code goes here
-    return NULL;
+	size_t size = vector_size(input->str_vec); 
+    char* result = malloc((size + 1) * sizeof(char));
+	size_t i;
+	for (i = 0; i < size; i++) {
+		result[i] = *((char*) vector_get(input->str_vec, i));
+	}
+	result[size] = '\0';
+	return result;
 }
 
 int sstring_append(sstring *this, sstring *addition) {
     // your code goes here
-    return -1;
+	size_t new_size = vector_size(this->str_vec) + vector_size(addition->str_vec);
+	vector_reserve(this->str_vec, new_size);
+	size_t i;
+	for (i = 0; i < vector_size(addition->str_vec); i++) {
+		vector_push_back(this->str_vec, vector_get(addition->str_vec, i));
+	}
+	return new_size;
 }
 
 vector *sstring_split(sstring *this, char delimiter) {
--- vector/sstring_test.c
+++ vector/sstring_test.c
@@ -4,8 +4,40 @@
  */
  
 #include "sstring.h"
-
+#include "vector.h"
 int main(int argc, char *argv[]) {
     // TODO create some tests
-    return 0;
+	// TEST 1
+	char* input_1 = "Test 1 My day is awful because 241";
+	sstring* test_1 = cstr_to_sstring(input_1);
+	char* test_1_return = sstring_to_cstr(test_1);
+	printf("%s\n", test_1_return);	
+
+	// TEST 2
+	sstring* input_2_first = cstr_to_sstring("My best friend is: ");
+	sstring* input_2_second = cstr_to_sstring("Duoduo. And Duoduo is beautiful");
+	sstring_append(input_2_first, input_2_second);
+	char* test_2_return = sstring_to_cstr(input_2_first);
+	printf("%s\n", test_2_return);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
 }
