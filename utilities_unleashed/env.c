553976d702e994ae5fcda046340b88d064c953a0
Zaiyan Xu <zxu43@illinois.edu>
2019-02-05 17:52:11 -0600

    file submission

--- utilities_unleashed/env.c
+++ utilities_unleashed/env.c
@@ -5,7 +5,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 
+#include "format.h"
 int ref_locator(char* str) {
 	char* ref_ptr = strchr(str, '%');
 	if (ref_ptr) {
@@ -39,12 +43,21 @@ char** arg_parser(char* str) {
 		result[1] = strndup(str, equal_position);
 		result[2] = strdup(str + equal_position + 1);
 		return result;
+	} else if (strstr(str, "--")) {
+		char** result = malloc(3 * sizeof(char*));
+		result[0] = strdup("dash");
+		result[1] = NULL;
+		result[2] = NULL;
+		return result;
 	} else {
 		return NULL;
 	}
 }
 
 bool arg_validator(int argc, char** argv) {
+	if (argc < 3) {
+		return false;
+	}
 	size_t i;
 	size_t j;
 	size_t count_per = 0;
@@ -76,15 +89,43 @@ bool arg_validator(int argc, char** argv) {
 
 int main(int argc, char *argv[]) {
 	bool valid = arg_validator(argc, argv);
-	printf("is it valid?: %d\n", valid);
-	size_t i;
-	if (valid) {
+	if (!valid) {
+		print_env_usage();
+		exit(1);
+	}
+	int status;
+	pid_t pid = fork();
+	if (pid < 0) { //Failed
+		print_fork_failed();
+		exit(1);
+	} else if (pid == 0) { // child case
+		char** arg;
+		size_t i;
 		for (i = 1; i < (size_t) argc; i++) {
-			char** parse_result = arg_parser(argv[i]);
-			if (parse_result) {
-				printf("ref?: %s\nkey: %s\nvalue: %s\n", parse_result[0], parse_result[1], parse_result[2]);
+			arg = arg_parser(argv[i]);	
+			if (!strcmp(arg[0], "ref")) {
+				char* env_val = getenv(arg[2]);
+				if (!env_val) {
+					env_val = "";
+				}
+				setenv(arg[1], env_val, 1);
+			} else if (!strcmp(arg[0], "equ")) {
+				setenv(arg[1], arg[2], 1);
+			} else if (!strcmp(arg[0], "dash")) {
+				break;
+			} else {
 			}
 		}
+		char* cmd = argv[i + 1];
+		printf("%s", cmd);
+		char** cmd_arg = argv + i + 1;
+		execvp(cmd, cmd_arg);
+		print_exec_failed();
+		exit(1);
+			
+	} else { //parent case
+		waitpid(pid, &status, 0);
 	}
+
 	return 0;		
 }
