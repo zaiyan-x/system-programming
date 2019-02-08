21b2358d230f0f3101e71c0aaca7099d81c35077
Zaiyan Xu <zxu43@illinois.edu>
2019-02-08 17:29:14 -0600

    file submission

--- shell/shell.c
+++ shell/shell.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <string.h>
  
 #include "format.h"
 #include "shell.h"
@@ -16,10 +17,10 @@
 extern char * optarg;
 extern int optind, opterr, optopt;
 
-char* HISTORY_FILE = NULL;
-char* COMMAND_FILE = NULL;
-char* HISTORY_FILE_PATH = NULL;
-char* COMMAND_FILE_PATH = NULL;
+static char* HISTORY_FILE = NULL;
+static char* COMMAND_FILE = NULL;
+static char* HISTORY_PATH = NULL;
+static char* COMMAND_PATH = NULL;
 
 typedef struct process {
     char *command;
@@ -39,16 +40,66 @@ bool argc_validator(int argc) {
 
 
 void signal_handler(int signal) {
-
+	if (signal == SIGINT) {
+		return;
+	} else if (signal == SIGCHLD) {
+		// TODO
+	} else {
+		return;
+	}
 }
 
 void shell_cleaner() {
+	if (HISTORY_FILE) {
+		free(HISTORY_FILE);
+		HISTORY_FILE = NULL;
+	}
+	if (COMMAND_FILE) {
+		free(COMMAND_FILE);
+		COMMAND_FILE = NULL;
+	}
+	if (HISTORY_PATH) {
+		free(HISTORY_PATH);
+		HISTORY_PATH = NULL;
+	}
+	if (COMMAND_PATH) {
+		free(COMMAND_PATH);
+		COMMAND_PATH = NULL;
+	}
 	return;
 }
 
-void option_parser(int argc, char** argv) {
+bool option_setup(int argc, char** argv) {
+	int opt;
+	while ((opt = getopt(argc, argv, "h;f:")) != -1) {
+		if (opt == 'h') {
+			if (HISTORY_FILE == NULL && optarg != NULL) {
+				HISTORY_FILE = strdup(optarg);
+			} else {
+				return false;
+			}
+		} else if (opt == 'f') {
+			if (COMMAND_FILE == NULL && optarg != NULL) {
+				COMMAND_FILE = strdup(optarg);
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	}
+	return true;
+}
+
+void command_dispatcher(char* cmd) {
 
 }
+
+void file_setup() {
+	if (HISTORY_FILE != NULL) {
+		if (access(HISTORY_FILE, R_OK | W_OK) != -1) {
+			HISTORY_PATH = (*get_full_path)(HISTORY_FILE);
+			//TODO	
 	
 int shell(int argc, char *argv[]) {
 	if (!argc_validator(argc)) {
@@ -60,6 +111,13 @@ int shell(int argc, char *argv[]) {
 	signal(SIGINT, signal_handler);
 	signal(SIGCHLD, signal_handler);
 
+	//get options
+	if (option_setup(argc, argv) == false) {
+		print_usage();
+		terminate_shell();
+	}	
+	puts(HISTORY_FILE);
+	puts(COMMAND_FILE);	
 	pid_t pid = getpid();
 
 	char* cmd = NULL;
@@ -74,8 +132,11 @@ int shell(int argc, char *argv[]) {
 			print_usage();
 			terminate_shell();	
 		}
-
-
+		//Change the newline char to NUL Char
+		cmd[strlen(cmd) - 1] = '\0';
+	
+		//Process commands		
+		command_processor(cmd);
 
 		//CLEAN UP - ready for next prompt
 		prompt_cleaner(cmd, cwd);
