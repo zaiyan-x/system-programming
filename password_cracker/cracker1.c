422b3f70d714cf86cad8bc6ae7e39224107c228e
Zaiyan Xu <zxu43@illinois.edu>
2019-03-07 22:52:06 -0600

    file submission

--- password_cracker/cracker1.c
+++ password_cracker/cracker1.c
@@ -14,12 +14,15 @@
 #include <stdio.h>
 
 #define MAX_PASSWORD 10000
+//#define NUM_OF_LOWER 26
+#define STRING_LEN 32
 
-queue * TASK_Q = NULL;
-pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+static queue * TASK_Q = NULL;
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 static size_t FINISHED = 0;
-static size_t STRING_LEN = 32;
 static size_t NUM_OF_LOWER = 26;
+static size_t ERROR = 0;
+static size_t TOTAL_TASK = 0;
 /*
  * task_line_parser parse and store info into separated stacks
  * It will free task_line, which is allocated by getline
@@ -53,25 +56,25 @@ void* crack_password (void* thread_id) {
 	//Initialize crypt
 	struct crypt_data cdata;
 	cdata.initialized = 0;
+	size_t tid = (size_t) thread_id;
 	while (true) {
+		//CRITICAL SECTION STARTS
 		//Check if it is finished
 		pthread_mutex_lock(&m);
 		if (FINISHED) {
 			pthread_mutex_unlock(&m);
 			break;
 		}
-		pthread_mutex_unlock(&m);
 
 		//Fetching from queue and Set things up
-		size_t tid = (size_t) thread_id;
 		char * task_line = (char *) queue_pull(TASK_Q);
 		if (task_line == NULL) { //lock and change FINISHED to 1
-			pthread_mutex_lock(&m);
 			FINISHED = 1;
 			pthread_mutex_unlock(&m);
 			break;
 		}
-		
+		pthread_mutex_unlock(&m);
+		//CRITICAL SECTION ENDS
 		char name[STRING_LEN];
 		char hash[STRING_LEN];
 		char password[STRING_LEN];
@@ -86,38 +89,47 @@ void* crack_password (void* thread_id) {
 		size_t known_length = unknown_start - password;
 		size_t password_length = strlen(password);
 		size_t unknown_length = password_length - known_length;
-		size_t num_of_comb = pow(unknown_length, NUM_OF_LOWER);
-		size_t i;
+		double num_of_comb = pow(NUM_OF_LOWER, unknown_length);
 		memset(unknown_start, 'a', unknown_length);
-
-		size_t curr_num_pos;
-		size_t curr_alpha_padding;
-		size_t curr_total;
+//		size_t curr_num_pos;
+//		size_t curr_alpha_padding;
+//		size_t curr_total;
 		const char * hashed;
-		
+		size_t i;	
+		//mega test
+//		printf("name is %s\n hash is %s\n password is %s\n known_length is %zu\n password_length is%zu\n unknown_length is%zu\n", name, hash, password, known_length, password_length, unknown_length); 
+//		printf("num_of_comb %f\n", num_of_comb);	
 		size_t FOUND = 0;
 		for (i = 0; i < num_of_comb; i++) {
-			curr_num_pos = known_length;
-			curr_total = i;
-			while (curr_num_pos < password_length) {
-				curr_alpha_padding = curr_total % 26;
-				password[curr_num_pos] += curr_alpha_padding;
-				curr_num_pos++;
-				curr_total = (curr_total - curr_alpha_padding) / 26;
-			}
+//			curr_num_pos = password_length - 1;
+//			curr_total = i;
+//			while (curr_total > 0 && curr_num_pos >= known_length) {
+//				curr_alpha_padding = curr_total % NUM_OF_LOWER;
+//				printf("curr_alpha_padding is %zu\n", curr_alpha_padding);
+//				password[curr_num_pos] += curr_alpha_padding;
+//				curr_num_pos--;
+//				curr_total = (curr_total - curr_alpha_padding) / NUM_OF_LOWER;
+//			}
+			if (i > 0) {
+				incrementString(unknown_start);
+			}			
+//			puts(password);
 			hashed = crypt_r(password, "xx", &cdata);
 			if (strcmp(hashed, hash) == 0) {
 				v1_print_thread_result(tid, name, password, i + 1, getThreadCPUTime(), 0);
 				FOUND = 1;
 				break;
 			}
-			memset(unknown_start, 'a', unknown_length);
+//			memset(unknown_start, 'a', unknown_length);
 		}
 		if (FOUND == 0) {
+			pthread_mutex_lock(&m);
+			ERROR++;
+			pthread_mutex_unlock(&m);
 			v1_print_thread_result(tid, name, password, num_of_comb, getThreadCPUTime(), 1);
 		}
 	}
-	return NULL;
+	pthread_exit(NULL);
 }
 
 
@@ -132,10 +144,14 @@ int start(size_t thread_count) {
 	while ((len = getline(&task_line, &task_line_size, stdin)) != -1) {
 		task_line[len - 1] = '\0';
 		queue_push(TASK_Q, task_line);
+		TOTAL_TASK++;
 		//Reset, accepting new stdin
 		task_line = NULL;
 		task_line_size = 0;	
 	}
+	//WARNING: Free the last call to getline
+	free(task_line);
+	task_line = NULL;
 	
 	queue_push(TASK_Q, NULL);
 	
@@ -150,5 +166,8 @@ int start(size_t thread_count) {
 	for (i = 0; i < thread_count; i++) {
 		pthread_join(threads[i], NULL);
 	}
+	pthread_mutex_destroy(&m);
+	queue_destroy(TASK_Q);
+	v1_print_summary(TOTAL_TASK - ERROR, ERROR);
 	return 0;
 }
--- password_cracker/cracker2.c
+++ password_cracker/cracker2.c
@@ -6,10 +6,229 @@
 #include "cracker2.h"
 #include "format.h"
 #include "utils.h"
+#include "queue.h"
+
+#include <math.h>
+#include <crypt.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_PASSWORD 10000
+#define STRING_LEN 32
+
+static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+static pthread_barrier_t b;
+static pthread_barrier_t B;
+static size_t THREAD_TOTAL = 0;
+static size_t TOTAL_TASK = 0;
+static char* NAME[MAX_PASSWORD];
+static char* HASH[MAX_PASSWORD];
+static char* PASSWORD[MAX_PASSWORD];
+static char CRACKED_PASSWORD[STRING_LEN];
+static char CRACKED_NAME[STRING_LEN];
+static size_t CURR_TOTAL_HASH = 0;
+static size_t CURR_TASK = 0;
+static bool FOUND_SYNC = false;
+static bool TASK_FINISHED = false;
+/*
+ * task_line_parser parse and store info into separated stacks
+ * It will free task_line, which is allocated by getline
+*/
+void task_line_parser(char * name, char * hash, char * password, char * task_line) {
+	char * target_pos = NULL;
+	char * curr_pos = task_line;
+	size_t cpy_len = 0;
+	target_pos = strchr(curr_pos, ' ');
+	cpy_len = target_pos - curr_pos;
+	strncpy(name, curr_pos, cpy_len);
+	name[cpy_len] = '\0';
+	curr_pos = target_pos + 1;
+	
+	target_pos = strchr(curr_pos, ' ');
+	cpy_len = target_pos - curr_pos;
+	strncpy(hash, curr_pos, cpy_len);
+	hash[cpy_len] = '\0';
+	curr_pos = target_pos + 1;
+	
+	target_pos = strchr(curr_pos, '\0');
+	cpy_len = target_pos - curr_pos;
+	strncpy(password, curr_pos, cpy_len);
+	password[cpy_len] = '\0';
+	
+	free(task_line);
+	task_line = NULL;
+}
+
+void heap_cleaner(size_t i) {
+	free(NAME[i]);
+	free(HASH[i]);
+	free(PASSWORD[i]);
+	NAME[i] = NULL;
+	HASH[i] = NULL;
+	PASSWORD[i] = NULL;
+	return;
+}
+
+void* crack_password(void* thread_id) {
+	//Initialize crypt and thread
+	struct crypt_data cdata;
+	cdata.initialized = 0;
+	size_t tid = (size_t) thread_id;
+
+	pthread_barrier_wait(&B); //Release MAIN to walk in for loop
+	pthread_barrier_wait(&B); //waiting for Main commands
+	/////////////////////
+	//   START TASK    //
+	/////////////////////
+	while (true) {
+		pthread_mutex_lock(&m);
+		size_t curr_task = CURR_TASK;
+		if (TASK_FINISHED) {
+			pthread_mutex_unlock(&m);
+			break;
+		}
+		pthread_mutex_unlock(&m);
+
+		char name[STRING_LEN];
+		char hash[STRING_LEN];
+		char password[STRING_LEN];
+		strcpy(name, NAME[curr_task]);
+		strcpy(hash, HASH[curr_task]);
+		strcpy(password, PASSWORD[curr_task]);
+		
+		char * unknown_start = strchr(password, '.');
+		size_t known_length = unknown_start - password;
+		size_t password_length = strlen(password);
+		size_t unknown_length = password_length - known_length;
+		long start_index = 0;
+		long count = 0;
+		getSubrange(unknown_length, THREAD_TOTAL, tid, &start_index, &count);
+
+		//Get thread starting string
+		setStringPosition(unknown_start, start_index);
+
+		//STARTING CRACKING !
+		pthread_mutex_lock(&m);
+		v2_print_thread_start(tid, name, start_index, password);
+		pthread_mutex_unlock(&m);
+
+		pthread_barrier_wait(&b);
+
+		long i;
+		const char* hashed;
+		size_t total_hashes = 0;
+		bool FOUND = false;
+		bool INTERUPT = false;	
+		for (i = 0; i < count; i++) {
+			pthread_mutex_lock(&m);
+			if (FOUND_SYNC == true) {
+				CURR_TOTAL_HASH += total_hashes;
+				pthread_mutex_unlock(&m);
+				INTERUPT = true;
+				break;
+			}
+			pthread_mutex_unlock(&m);
+			if (i > 0) {
+				incrementString(unknown_start);
+			}
+//			puts(password);
+			total_hashes++;
+			hashed = crypt_r(password, "xx", &cdata);
+			if (strcmp(hashed, hash) == 0) {
+				FOUND = true;
+				break;
+			}
+		}
+		if (FOUND == true) {
+			pthread_mutex_lock(&m);
+			FOUND_SYNC = true;
+			strcpy(CRACKED_PASSWORD, password);
+			strcpy(CRACKED_NAME, name);
+			CURR_TOTAL_HASH += total_hashes;
+			v2_print_thread_result(tid, total_hashes, 0);
+			pthread_mutex_unlock(&m);
+		}
+		if (FOUND == false && INTERUPT == false) {
+			pthread_mutex_lock(&m);
+			CURR_TOTAL_HASH += total_hashes;
+			v2_print_thread_result(tid, total_hashes, 2);
+			pthread_mutex_unlock(&m);
+		}
+		if (FOUND == false && INTERUPT == true) {
+			pthread_mutex_lock(&m);
+			CURR_TOTAL_HASH += total_hashes;
+			v2_print_thread_result(tid, total_hashes, 1);
+			pthread_mutex_unlock(&m);
+		}
+		pthread_barrier_wait(&B); //waiting for parents to print summary
+		pthread_barrier_wait(&B); //waiting for parents to release
+	}
+	pthread_exit(NULL);
+}
 
 int start(size_t thread_count) {
-    // TODO your code here, make sure to use thread_count!
-    // Remember to ONLY crack passwords in other threads
-    return 0; // DO NOT change the return code since AG uses it to check if your
-              // program exited normally
+	//Initialize
+	pthread_barrier_init(&b, NULL, thread_count);
+	pthread_barrier_init(&B, NULL, thread_count + 1);
+	THREAD_TOTAL = thread_count;
+
+	//Listening to inputs
+	char * task_line = NULL;
+	size_t task_line_size = 0;
+	ssize_t len;
+	while ((len = getline(&task_line, &task_line_size, stdin)) != -1) {
+		task_line[len - 1] = '\0';
+		NAME[TOTAL_TASK] = malloc(STRING_LEN * sizeof(char));
+		HASH[TOTAL_TASK]  = malloc(STRING_LEN * sizeof(char));
+		PASSWORD[TOTAL_TASK] = malloc(STRING_LEN * sizeof(char));
+		task_line_parser(NAME[TOTAL_TASK], HASH[TOTAL_TASK], PASSWORD[TOTAL_TASK], task_line);
+		TOTAL_TASK++;
+		//Reset, accepting new stdin
+		task_line = NULL;
+		task_line_size = 0;	
+	}
+	//WARNING: Free the last call to getline
+	free(task_line);
+	task_line = NULL;
+
+	//Start threads (stdin readin ends here)
+	size_t i;
+	pthread_t threads[thread_count];
+	for (i = 0; i < thread_count; i++) {
+		pthread_create(&threads[i], NULL, crack_password, (void*) (i + 1));
+	}
+	//Waiting for Child Thread Initialization
+	pthread_barrier_wait(&B);
+	for (i = 0; i <= TOTAL_TASK; i++) {
+		if (i == TOTAL_TASK) {
+			TASK_FINISHED = true;
+			break;
+		}
+		double start_time = getTime();
+		double start_cpu_time = getCPUTime();
+		v2_print_start_user(NAME[i]);
+		CURR_TASK = i;
+		CURR_TOTAL_HASH = 0;
+		pthread_barrier_wait(&B); //Releasing children to crack
+		pthread_barrier_wait(&B); //waiting for children result
+		if (FOUND_SYNC == true) {
+			FOUND_SYNC = false;
+			v2_print_summary(CRACKED_NAME, CRACKED_PASSWORD, CURR_TOTAL_HASH, getTime() - start_time, getCPUTime() - start_cpu_time, 0);
+		} else { //FOUND_SYNC == false
+			v2_print_summary(CRACKED_NAME, CRACKED_PASSWORD, CURR_TOTAL_HASH, getTime() - start_time, getCPUTime() - start_cpu_time, 1);
+		}
+		heap_cleaner(i);
+	}
+	pthread_barrier_wait(&B); //Releasing threads for the LAST time;	
+	
+	
+	//Clean Up
+	for (i = 0; i < thread_count; i++) {
+		pthread_join(threads[i], NULL);
+	}
+	pthread_mutex_destroy(&m);
+	pthread_barrier_destroy(&b);
+	pthread_barrier_destroy(&B);
+		
+	return 0;
 }
