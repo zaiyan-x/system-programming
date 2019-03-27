a9c0ce7db1db1b44b9d39ccd75aa4aab43463ae2
Zaiyan Xu <zxu43@illinois.edu>
2019-03-27 16:51:11 -0500

    file submission

--- mapreduce/mapreduce.c
+++ mapreduce/mapreduce.c
@@ -1,21 +1,20 @@
 /**
  * mapreduce Lab
  * CS 241 - Spring 2019
- * Collab with Eric Wang - wcwang2 :))))
+ * Collab with Eric Wang - wcwang2 :)
  */
  
+
 #include "utils.h"
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <unistd.h> 
 #include <string.h> 
+#include <sys/types.h> 
 #include <sys/wait.h> 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <fcntl.h> 
 
 int main(int argc, char **argv) {
-	
 	if (argc != 6) { 
 		print_usage(); 
 		return 0; 
@@ -41,22 +40,23 @@ int main(int argc, char **argv) {
 	descriptors_add(reducer_pipe[1]); 
     
 	// Open the output file.
-	int output_fd = open(output_file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH); 
-	descriptors_add(output_fd); 
+	int f_output = open(output_file, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH); 
+	descriptors_add(f_output); 
 
     // Start a splitter process for each mapper.
+	pid_t splitter_pids[mapper_count]; 
 	for (int i = 0; i < mapper_count; i++) {  
-		pid_t splitter_pid = fork(); 
-		if (splitter_pid == 0) { // child
-			dup2(mapper_pipe[i][1], 1);
-			char num_of_mapper[10];
-			snprintf(num_of_mapper, 10, "%d", mapper_count);
-			char current_mapper[10];
-			snprintf(current_mapper, 10, "%d", i);
-			descriptors_closeall();
+		splitter_pids[i] = fork(); 
+		if (splitter_pids[i] == 0) { // child
+			dup2(mapper_pipe[i][1], 1); 
+			char num_of_mapper[10]; 
+			snprintf(num_of_mapper, 10, "%d", mapper_count); 
+			char current_mapper[10]; 
+			snprintf(current_mapper, 10, "%d", i); 
+			descriptors_closeall(); 
 			descriptors_destroy(); 
 			int status = execlp("./splitter", "./splitter", input_file, num_of_mapper, current_mapper, NULL); 
-			print_nonzero_exit_status("./splitter", status); 
+			if (status < 0) print_nonzero_exit_status("./splitter", status); 
 			exit(1); 
 		} 
 	} 
@@ -67,11 +67,11 @@ int main(int argc, char **argv) {
 		mapper_pids[i] = fork(); 
 		if (mapper_pids[i] == 0) { // child 
 			dup2(mapper_pipe[i][0], 0); 
-			dup2(reducer_pipe[1], 1);
-			descriptors_closeall();
+			dup2(reducer_pipe[1], 1); 
+			descriptors_closeall(); 
 			descriptors_destroy(); 
 			int status = execlp(mapper_executable, mapper_executable, NULL); 
-			print_nonzero_exit_status(mapper_executable, status); 
+			if (status < 0) print_nonzero_exit_status(mapper_executable, status); 
 			exit(1); 
 		} 
 	} 
@@ -80,23 +80,35 @@ int main(int argc, char **argv) {
 	pid_t reducer_pid = fork(); 
 	if (reducer_pid == 0) { // child 
 		dup2(reducer_pipe[0], 0); 
-		dup2(output_fd, 1);
-		descriptors_closeall();
-		descriptors_destroy();
+		dup2(f_output, 1); 
+		descriptors_closeall(); 
+		descriptors_destroy(); 
 		int status = execlp(reducer_executable, reducer_executable, NULL); 
-		print_nonzero_exit_status(reducer_executable, status); 
+		if (status < 0) print_nonzero_exit_status(reducer_executable, status); 
 		exit(1); 
 	} 
+	
+	descriptors_closeall(); 
+	descriptors_destroy(); 
 
 	// Wait for the reducer to finish 
+	for (int i = 0; i < mapper_count; i++) { 
+		if (splitter_pids[i] > 0) { 
+			int status; 
+			waitpid(splitter_pids[i], &status, 0); 
+		} 
+	} 
+	for (int i = 0; i < mapper_count; i++) { 
+		if (mapper_pids[i] > 0) { 
+			int status; 
+			waitpid(mapper_pids[i], &status, 0); 
+		} 
+	} 
+	if (reducer_pid > 0) { 
+		int status; 
+		waitpid(reducer_pid, &status, 0); 
+	} 
 
-	descriptors_closeall();
-	descriptors_destroy();
-
-	while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {
-		continue;
-	}
-	
     // Count the number of lines in the output file.
 	print_num_lines(output_file); 
     return 0;
