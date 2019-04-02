80e1ead02ac8cf30b5dd170fac824cb402a9023f
Zaiyan Xu <zxu43@illinois.edu>
2019-04-01 21:47:34 -0500

    file submission

--- charming_chatroom/utils.c
+++ charming_chatroom/utils.c
@@ -6,6 +6,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
 
 #include "utils.h"
 static const size_t MESSAGE_SIZE_DIGITS = 4;
@@ -31,16 +34,42 @@ ssize_t get_message_size(int socket) {
 
 // You may assume size won't be larger than a 4 byte integer
 ssize_t write_message_size(size_t size, int socket) {
-    // Your code here
-    return 9001;
+	ssize_t message_size = htonl(size);
+	ssize_t byte_written = write_all_to_socket(socket, (char*) &message_size, MESSAGE_SIZE_DIGITS);
+	if (byte_written == 0 || byte_written == -1) {
+		return byte_written;
+	}
+    return message_size;
 }
 
 ssize_t read_all_from_socket(int socket, char *buffer, size_t count) {
-    // Your Code Here
-    return 9001;
+	ssize_t byte_to_read = count;
+	ssize_t byte_read = 0;
+	while (byte_to_read) {
+		byte_read = read(socket, buffer + count - byte_to_read, byte_to_read);
+		if (byte_read <= 0) {
+			if (byte_read == -1 && errno == EINTR) {
+				continue;
+			}
+		}
+		byte_to_read -= byte_read;
+	}
+	return count;
 }
 
 ssize_t write_all_to_socket(int socket, const char *buffer, size_t count) {
-    // Your Code Here
-    return 9001;
+    ssize_t byte_to_write = count;
+	ssize_t byte_written = 0;
+	while (byte_to_write) {
+		byte_written = write(socket, buffer + count - byte_to_write, byte_to_write);
+		if (byte_written <= 0) {
+			if (byte_written == -1 && errno == EINTR) {
+				continue;
+			}
+			perror(strerror(byte_written));
+			return byte_written;
+		}
+		byte_to_write -= byte_written;
+	}
+	return count;
 }
