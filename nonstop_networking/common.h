e4e6c493e9a30b0bcc3da72fcd311563ad5a7119
Xu, Ryan <zxu43@illinois.edu>
2019-04-14 12:13:06 -0500

    Update common.h

--- nonstop_networking/common.h
+++ nonstop_networking/common.h
@@ -17,6 +17,7 @@ typedef enum { GET, PUT, DELETE, LIST, V_UNKNOWN } verb;
 ssize_t client_write_all_to_socket(int socket, const char * buffer, size_t count); 
 ssize_t client_read_line_from_socket(int socket, char * buffer, size_t count);
 ssize_t client_read_all_from_socket(int socket, char * buffer, size_t count);
+void handle_return_value(ssize_t byte_executed, size_t byte_to_execute, size_t total_byte_to_execute);
 
 //Helper functions
 char** parse_args(int argc, char** argv);
