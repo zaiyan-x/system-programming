5645018124265b869d7ef5cc2dc51964264c6032
Xu, Ryan <zxu43@illinois.edu>
2019-04-21 15:54:11 -0500

    Update server.c

--- nonstop_networking/server.c
+++ nonstop_networking/server.c
@@ -139,7 +139,10 @@ void setup_delete(int client_fd, client* current_client) {
 void setup_list(int client_fd, client* current_client) {
 	size_t total_file_size = 0;
 	size_t current_file_size = 0;
-	FILE * list_temp_file = open_file("temp", "w");	
+	char temp_file_name[MAX_FILENAME_SIZE];
+	memset(temp_file_name, 0, MAX_FILENAME_SIZE);
+	sprintf(temp_file_name, "%d temp", client_fd);
+	FILE * list_temp_file = open_file(temp_file_name, "w");	
 	current_client->file = list_temp_file;
 	char * current_filename = NULL;
 	
